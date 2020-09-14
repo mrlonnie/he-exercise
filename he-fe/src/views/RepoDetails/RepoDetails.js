c61c130a3cbaa6961b9ed60814271bc48d90c0a1
mrlonnie <lonnie.moy@gmail.com>
2020-09-13 18:29:01 -0600

    move general details into its own component

--- he-fe/src/views/RepoDetails/RepoDetails.js
+++ he-fe/src/views/RepoDetails/RepoDetails.js
@@ -4,13 +4,9 @@ import {
   Accordion,
   AccordionDetails,
   AccordionSummary,
-  Avatar,
   Breadcrumbs,
   Container,
-  List,
   ListItem,
-  ListItemIcon,
-  ListItemText,
   Typography,
 } from '@material-ui/core';
 import {
@@ -18,11 +14,7 @@ import {
   useParams
 } from "react-router-dom";
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import StarIcon from '@material-ui/icons/Star';
-import VisibilityIcon from '@material-ui/icons/Visibility';
-import CallSplitIcon from '@material-ui/icons/CallSplit';
-import InfoIcon from '@material-ui/icons/Info';
+import GeneralDetailsList from './GeneralDetailsList';
 import API from '../../data/api';
 
 const RepoDetails = () => {
@@ -54,13 +46,7 @@ const RepoDetails = () => {
     }
   }, [repo, owner]);
 
-  /**
-   * ListItemLink
-   * @param props 
-   */
-  const ListItemLink = (props) => {
-    return <ListItem button component="a" {...props} />;
-  }
+
 
   /**
    * handleSetAccordion 
@@ -72,22 +58,23 @@ const RepoDetails = () => {
   };
 
   /**
-   * generateDetails 
+   * generateRawDetails 
    */
-  const generateDetails = () => {
+  const generateRawDetails = () => {
     return(
       <div><pre>{JSON.stringify(repoDetails, null, 2) }</pre></div>
     )
   }
 
+
   return (
     <Container>
-    <Breadcrumbs aria-label="breadcrumb">
-      <RouterLink to="/">
-        Search Form
-      </RouterLink>
-       <Typography color="textPrimary">Details</Typography>
-    </Breadcrumbs>
+      <Breadcrumbs aria-label="breadcrumb">
+        <RouterLink to="/">
+          Search Form
+        </RouterLink>
+        <Typography color="textPrimary">Details</Typography>
+      </Breadcrumbs>
       <Accordion
         square
         expanded={activeAccordion === 'brief'}
@@ -97,63 +84,7 @@ const RepoDetails = () => {
           Repo Details - Brief
         </AccordionSummary>
         <AccordionDetails>
-            <List>
-              {/* Detail Row - Repo Name | Owner Avatar | Owner Name */}
-              <ListItemLink href={repoDetails.html_url}>
-                <ListItemIcon>
-                  <GitHubIcon />
-                </ListItemIcon>
-                <ListItemText primary={repoDetails.name} />
-                <ListItemIcon>
-                  <Avatar src={ repoDetails.owner && repoDetails.owner.avatar_url}/>
-                </ListItemIcon>
-                <ListItemText primary={repoDetails.owner &&repoDetails.owner.login} />
-              </ListItemLink>
-              {/* Detail Row - Waters | Stars | Forks | Isses */}
-              <ListItem>
-                <ListItemIcon>
-                  <VisibilityIcon />
-                </ListItemIcon>
-                <ListItemText primary={repoDetails.watchers_count} />
-                <ListItemIcon>
-                  <StarIcon />
-                </ListItemIcon>
-                <ListItemText primary={repoDetails.stargazers_count} />
-                <ListItemIcon>
-                  <CallSplitIcon />
-                </ListItemIcon>
-                <ListItemText primary={repoDetails.forks_count} />
-                <ListItemIcon>
-                  <InfoIcon />
-                </ListItemIcon>
-                <ListItemText primary={repoDetails.open_issues} />
-              </ListItem>
-              {/* Detail Row - Description */}
-              {
-                 repoDetails.description &&
-                 <ListItem>
-                  Description: {repoDetails.description}
-                </ListItem>
-               }
-               {/* Detail Row - Language */}
-              {
-                 repoDetails.language &&
-                 <ListItem>
-                  Language: {repoDetails.language}
-                </ListItem>
-               }
-
-               {/* Detail Row - URL | Clone URL | SSH URL */}
-               <ListItemLink href={repoDetails.html_url}>
-                  Link: {repoDetails.html_url}
-                </ListItemLink>
-               <ListItemLink href={repoDetails.clone_url}>
-                  Clone URL: {repoDetails.clone_url}
-                </ListItemLink>
-               <ListItemLink href={repoDetails.clone_url}>
-                  SSH URL: {repoDetails.ssh_url}
-                </ListItemLink>
-            </List>
+          <GeneralDetailsList repoDetails={repoDetails} />
         </AccordionDetails>
       </Accordion>
 
@@ -170,7 +101,7 @@ const RepoDetails = () => {
           Raw Details
         </AccordionSummary>
         <AccordionDetails>
-          {generateDetails()}
+          {generateRawDetails()}
         </AccordionDetails>
       </Accordion>
     </Container>
